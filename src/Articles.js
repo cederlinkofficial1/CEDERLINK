875112c1b1fda9078d379e33e8ec063a9793a502
Anoop Shukla <anoop.shukla@gmail.com>
2025-09-22 02:41:40 +0530

    newStart

--- src/Articles.js
+++
@@ -1,8 +0,0 @@
-const Articles = () => {
-    return(
-        <div>
-            <h1>Nothing in Article right now</h1>
-        </div>
-    );
-};
-export default Articles;
--- src/Contact.js
+++
@@ -1,9 +0,0 @@
-const Contact = () => {
-    return(
-        <div>
-            <h1>Contact : Nikhil Pal</h1>
-        </div>
-    );
-};
-
-export default Contact;
